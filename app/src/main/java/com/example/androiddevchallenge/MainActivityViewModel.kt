679218381908347f85c9b20df1f8f1b05ec1fd81
david <cd4ve@hotmail.co.uk>
2021-03-01 21:28:00 +0000

    Finish app

--- app/src/main/java/com/example/androiddevchallenge/MainActivityViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivityViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.androiddevchallenge.core.actions.GetAllDogsAction
+import com.example.androiddevchallenge.core.actions.GetDogAction
 import com.example.androiddevchallenge.core.types.Dog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,13 +15,22 @@ class MainActivityViewModel(app : Application) : AndroidViewModel(app) {
 
     private val coroutine = CoroutineScope(Dispatchers.IO)
     private val getAllDogs = GetAllDogsAction()
+    private val getDog = GetDogAction()
 
     val dogs = MutableLiveData<List<Dog>>()
+    val dog = MutableLiveData<Event<Dog>>()
 
     fun loadDogs() {
         coroutine.launch {
             delay(3000) //Pretend we're loading data
             dogs.postValue(getAllDogs.invoke())
+        }
+    }
+
+    fun getDog(id : Long) {
+        coroutine.launch {
+            delay(500) //Pretend we're loading data
+            dog.postValue(Event(getDog.invoke(id)))
         }
     }
 
--- app/src/main/java/com/example/androiddevchallenge/ui/composables/Composables.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/composables/Composables.kt
@@ -1,34 +1,32 @@
 package com.example.androiddevchallenge.ui.composables
 
-import android.view.Gravity
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.HorizontalAlignmentLine
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.RecordClick
+import com.example.androiddevchallenge.ResourceConverter
 import com.example.androiddevchallenge.core.types.Dog
+import com.example.androiddevchallenge.core.types.Sex
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.typography
 
-// Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
-    }
-}
-
-@Composable
-fun LoadingList(){
+fun LoadingIndicator(){
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -38,4 +36,187 @@ fun LoadingList(){
     }
 }
 
+@Composable
+fun DogList(dogs: List<Dog>, clickEvent: RecordClick) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())) {
+        dogs.forEach { dog ->
+            DogRow(dog, clickEvent)
+        }
+    }
+}
 
+
+@Composable
+fun FirstScreen(
+    dogs : List<Dog>,
+    recordClick: RecordClick) {
+    MyTheme {
+        DogList(dogs, recordClick)
+    }
+}
+
+@Composable
+fun DogRow(dog: Dog, clickEvent: RecordClick) {
+    Card(
+        elevation = 16.dp,
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = MaterialTheme.colors.surface,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .clickable {
+                clickEvent.click(dog.id)
+            }) {
+        Row(modifier = Modifier.padding(0.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            ProfilePic(dog)
+            RowContent(dog)
+        }
+    }
+}
+
+@Composable
+fun ProfilePic(dog : Dog) {
+    Image(
+        painter = painterResource(ResourceConverter().getDrawableId(dog.fileName)),
+        contentDescription = "profile_pic",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(95.dp)
+            .padding(8.dp)
+            .clip(CircleShape)
+            .border(4.dp, color = Color.Blue, shape = CircleShape)
+    )
+}
+
+@Composable
+fun RowContent(dog: Dog) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(text = dog.name, style = typography.h4)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                RowLocation(dog)
+                RowAge(dog)
+            }
+        }
+        RowDescription(dog)
+    }
+}
+
+@Composable
+fun RowLocation(dog: Dog) {
+    Card(elevation = 16.dp, backgroundColor = Color.Blue, modifier = Modifier.padding(4.dp)) {
+        Text(dog.location, color = Color.White, modifier = Modifier.padding(4.dp))
+    }
+}
+
+
+@Composable
+fun RowAge(dog: Dog) {
+    Card(
+        elevation = 16.dp,
+        backgroundColor = Color.Blue, modifier = Modifier.padding(4.dp)) {
+        Text("${dog.age} yrs", color = Color.White, modifier = Modifier.padding(4.dp))
+    }
+}
+
+@Composable
+fun RowDescription(dog: Dog) {
+    Text(text = dog.description,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2, style = typography.h5)
+}
+
+@Composable
+fun DogDetailedView(dog : Dog) {
+    Scaffold (
+        content = {
+            DetailsBody(dog)
+        },
+        floatingActionButton = {
+            Fab()
+        }
+    )
+}
+
+@Composable
+fun DetailsBody(dog: Dog) {
+    Column {
+        Box {
+            ContentImage(dog)
+            DogTopInfo(dog)
+        }
+        DogDetails(dog)
+    }
+}
+
+@Composable
+private fun ContentImage(dog : Dog) {
+    Image(
+        painter = painterResource(ResourceConverter().getDrawableId(dog.fileName)),
+        contentDescription = "profile_pic",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    )
+}
+
+@Composable
+fun DogTopInfo(dog: Dog) {
+    Box(modifier = Modifier.padding(8.dp)) {
+        Card(shape = RoundedCornerShape(8.dp), elevation = 32.dp,
+            modifier = Modifier.padding(4.dp), contentColor = Color.White,
+            backgroundColor = Color.Blue) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "${dog.age} yrs", modifier = Modifier.padding(8.dp))
+                Text(text = dog.location, modifier = Modifier.padding(8.dp))
+                Image(
+                    painter = if (dog.sex == Sex.Male)
+                        painterResource(R.drawable.gender_male)
+                    else
+                        painterResource(R.drawable.gender_female),
+                    contentDescription = "profile_pic",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                        .padding(4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DogDetails(dog: Dog) {
+    Card(modifier = Modifier.padding(16.dp), elevation = 8.dp) {
+        Column {
+            Text(
+                modifier = Modifier.padding(8.dp), text = dog.name, style = typography.h4)
+            Text(
+                modifier = Modifier.padding(8.dp), text = dog.description, style = typography.body2)
+        }
+    }
+}
+
+@Composable
+fun Fab() {
+    ExtendedFloatingActionButton(
+        onClick = { /*Do nothing as we can't really adopt anything*/ },
+        icon = { Icon(
+            Icons.Filled.Favorite,"",
+            tint = Color.White) },
+        backgroundColor = Color.Blue,
+        text = {Text(text = "Adopt me", color = Color.White)})
+}
